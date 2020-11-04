323425b877160a7bcb72102e70f6221b68771eba
J. Senecal <jacobsenecal@yahoo.com>
2020-11-04 08:38:48 -0700

    passing basic test

--- service-rss-feed/tests/health_check.rs
+++ service-rss-feed/tests/health_check.rs
@@ -1,4 +1,4 @@
-use tokio; 
+use std::net::TcpListener;
 
 // `actix_rt::test` is the testing equivalent of `actix_rt::main`.
 // It also spares you from having to specify the `#[test]` attribute.
@@ -6,12 +6,11 @@ use tokio;
 // `cargo expand --test health_check` (<- name of the test file)
 #[actix_rt::test]
 async fn health_check_works() {
-    spawn_app();
-
+    let address = spawn_app();
     let client = reqwest::Client::new();
 
     let response = client
-        .get("http://127.0.0.1:8000/health_check")
+        .get(&format!("{}/health_check", &address))
         .send()
         .await
         .expect("Failed to execute request.");
@@ -24,8 +23,14 @@ async fn health_check_works() {
 // We are also running tests, so it is not worth it to propagate errors:
 // if we fail to perform the required setup we can just panic and crash
 // all the things.
-async fn spawn_app() {
-    let server = service_rss_feed::run().expect("Failed to bind address");
+fn spawn_app() -> String {
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+    // We retrieve the port assigned to us by the OS
+    let port = listener.local_addr().unwrap().port();
+    let server = service_rss_feed::run(listener).expect("Failed to bind address");
     // launch the server as a background task 
     let _ = tokio::spawn(server);
+    
+    // we return the application address to the caller
+    format!("http://127.0.0.1:{}", port)
 }
