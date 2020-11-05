ffcbbab9baf45e8fd38d916fb50bc5fb24cc4828
J. Senecal <jacobsenecal@yahoo.com>
2020-11-05 08:53:36 -0700

    pass in connection

--- service-rss-feed/src/main.rs
+++ service-rss-feed/src/main.rs
@@ -1,13 +1,17 @@
 use service_rss_feed::startup::run; 
 use service_rss_feed::configuration::get_configuration;
 use std::net::TcpListener;
+use sqlx::{ Connection, PgConnection };
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
     let configuration = get_configuration().expect("Failed to read configuration") ;
-
+    let conn = PgConnection::connect(&configuration.database.connection_string())
+        .await
+        .expect("Failed to connect to Postgres.");
+    let address = format!("127.0.0.1:{}", configuration.application_port);
     // Bubble up the io::Error if we failed to bind the address
     // Otherwise call .await on our Server
-    let address = TcpListener::bind("127.0.0.1:8000")?;
-    run(address)?.await
+    let listener = TcpListener::bind(address)?;
+    run(listener, conn)?.await
 }
--- service-rss-feed/src/startup.rs
+++ service-rss-feed/src/startup.rs
@@ -1,13 +1,18 @@
 use actix_web::{web, App, HttpServer};
 use actix_web::dev::Server; 
 use std::net::TcpListener;
+use std::sync::Arc;
+use sqlx::PgConnection;
 use crate::routes::{health_check, register};
 
-pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| {
+pub fn run(listener: TcpListener, conn: PgConnection) -> Result<Server, std::io::Error> {
+    let conn = Arc::new(conn);
+
+    let server = HttpServer::new(move || {
         App::new()
             .route("/health-check", web::get().to(health_check))
             .route("/register-user", web::post().to(register))
+            .data(conn.clone())
     })
     .listen(listener)?
     .run();
