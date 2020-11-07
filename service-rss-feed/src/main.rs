99fc09a8d1eef9a998e17b3fc52294576fe80472
J. Senecal <jacobsenecal@yahoo.com>
2020-11-07 10:13:19 -0700

    update config add docker

--- service-rss-feed/src/main.rs
+++ service-rss-feed/src/main.rs
@@ -2,7 +2,7 @@ use service_rss_feed::startup::run;
 use service_rss_feed::configuration::get_configuration;
 use service_rss_feed::telemetry::{ get_subscriber, init_subscriber };
 use std::net::TcpListener;
-use sqlx::PgPool;
+use sqlx::postgres::PgPoolOptions;
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
@@ -10,11 +10,16 @@ async fn main() -> std::io::Result<()> {
     init_subscriber(subscriber);
 
     let configuration = get_configuration().expect("Failed to read configuration") ;
-    let connection_pool = PgPool::connect(&configuration.database.connection_string())
+    let connection_pool = PgPoolOptions::new()
+        .connect_timeout(std::time::Duration::from_secs(2))
+        .connect(&configuration.database.connection_string())
         .await
         .expect("Failed to connect to Postgres.");
 
-    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let address = format!(
+        "{}:{}", 
+        configuration.application.host, configuration.application.port
+    );
     let listener = TcpListener::bind(address)?;
     run(listener, connection_pool)?.await
 }
