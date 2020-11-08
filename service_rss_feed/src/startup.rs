53f1e5fd414d1d80b1d73b7e6568850526128b83
J. Senecal <jacobsenecal@yahoo.com>
2020-11-08 15:53:59 -0700

    add session middleware

--- service_rss_feed/src/startup.rs
+++ service_rss_feed/src/startup.rs
@@ -1,5 +1,6 @@
 use actix_web::{web, App, HttpServer, FromRequest};
 use actix_web::dev::Server; 
+use actix_web::middleware::session::{RequestSession, SessionStorage, CookieSessionBackend};
 use std::net::TcpListener;
 use sqlx::PgPool;
 use tracing_actix_web::TracingLogger;
@@ -14,6 +15,11 @@ pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Er
             .app_data(String::configure(|cfg| {
                 cfg.limit(4096)
             }))
+            .middleware(
+                SessionStorage::new(
+                    CookieSessionBackend::signed(&[0; 32]).secure(false)
+                )
+            )
             .route("/health-check", web::get().to(health_check))
             .route("/register-user", web::post().to(register))
             .route("/save-feed", web::post().to(save_feed))
