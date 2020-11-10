d9424fad1bf48fa7cdc535b75ad7c7e1ff18eedf
jsen <senecal@mac>
2020-11-09 21:36:22 -0700

    find feeds for profile

---
+++ service_rss_feed/src/routes/return_feed.rs
@@ -0,0 +1,30 @@
+#[tracing::instrument](
+    name = "Finding subscribed feeds",
+    skip(pool, session)
+)]
+pub async fn get_subscribed_feeds(
+    session: Session,
+    pool: web::Data<PgPool>
+) -> Result<HttpResponse, HttpResponse> {
+    let profile_id: Option<Uuid> = session.get("profile_id")?;
+
+
+}
+
+pub async fn find_feeds_for_profile(profile_id: Uuid) -> Result<Vec<Uuid>, sqlx::Error> {
+    let result = sqlx::query!(
+        r#"
+        SELECT feed_id FROM subscription 
+        WHERE profile_id=$1
+        "#,
+        profile_id
+    )
+    .fetch(pool)
+    .await 
+    .map_err(|e| {
+        tracing::error!("Failed to execute query: {:?}", e);
+        e
+    })?
+
+    Ok(result)
+}
