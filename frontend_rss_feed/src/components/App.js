bf50f73d7731c25a1df595127fa7db98775ff2cc
jsen <senecal@mac>
2020-11-10 22:42:14 -0700

    define login form

--- frontend_rss_feed/src/components/App.js
+++ frontend_rss_feed/src/components/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Login from "./Login";
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
         <header>Login or Register</header>
+        <Login />
       </div>
     )
   }
--- frontend_rss_feed/src/components/Login.js
+++ frontend_rss_feed/src/components/Login.js
@@ -2,6 +2,13 @@ import React from "react";
 
 class Login extends React.Component {
     render () {
-
+        return (
+            <form className="login-form">
+                <input name="email" type="text" placeholder="Email"></input>
+                <input name="password" type="text" placeholder="Password"></input>
+            </form>
+        )
     }
 }
+
+export default Login;
