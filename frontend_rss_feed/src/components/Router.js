5370c00d6ec1e5b2ff528037f4b288c2706e2263
jsen <senecal@mac>
2020-11-14 18:17:52 -0700

    make private route

--- frontend_rss_feed/src/components/Router.js
+++ frontend_rss_feed/src/components/Router.js
@@ -1,10 +1,28 @@
-import React from "react";
+import React, { Component } from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import App from "./App";
 import Dashboard from "./Dashboard";
 import NotFound from "./NotFound";
 
+const PrivateRoute = ({ component: Component, ...rest}) => (
+    <Route {...rest} render={(props) => (
+        Auth.isAuthenticated === true 
+            ? <Component {...props} />
+            : <Redirect to='/' />
+    )} />
+)
+
 const Router = () => (
+    // function requireAuth(nextState, replace, next) {
+    //     if (!authenticated) {
+    //       replace({
+    //         pathname: "/login",
+    //         state: {nextPathname: nextState.location.pathname}
+    //       });
+    //     }
+    //     next();
+    // }
+
     <BrowserRouter>
         <Switch>
             <Route exact path="/" component={App} />
