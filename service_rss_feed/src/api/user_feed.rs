27bac45eee57ebf5a21c87b66ebd402c5dab131f
J. Senecal <jacobsenecal@yahoo.com>
2020-11-26 22:03:42 -0700

    add feed response object

---
+++ service_rss_feed/src/api/user_feed.rs
@@ -0,0 +1,11 @@
+pub struct UserFeed {
+
+}
+
+pub struct RssChannel {
+
+}
+
+pub struct Item {
+
+}
--- service_rss_feed/src/routes/return_feed.rs
+++ service_rss_feed/src/routes/return_feed.rs
@@ -33,6 +33,8 @@ pub async fn get_subscribed_feeds(
 
         Err(e) => return HttpResponse::InternalServerError().finish()
     }
+
+    
 }
 
 pub async fun get_feed_channel(feed_id: Uuid, pool: &PgPool) -> Result<Vec<String>, sqlx::Error> {
