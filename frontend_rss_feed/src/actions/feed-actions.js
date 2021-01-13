8a31fef36c9160908e8e5b04b96766a0657a129c
Jacob Senecal <jsenecal@figure.com>
2021-01-13 14:49:15 -0700

    add http actions

--- frontend_rss_feed/src/actions/feed-actions.js
+++ frontend_rss_feed/src/actions/feed-actions.js
@@ -1,8 +1,8 @@
 import { ajaxGet } from "./ajax";
 
-const FETCH = "FETCH";
+export const FETCH = "FETCH";
 
-const FEED_CONTEXT = "/get-feeds";
+const FEED_CONTEXT = "/api/v1/get-feeds";
 
 export const getFeeds = () => async (dispatch) => {
     ajaxGet(FETCH, dispatch, FEED_CONTEXT);
