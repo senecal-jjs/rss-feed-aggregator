b8a9f25d4f8bea95cfe8d1bb94892bdffff207ef
Jacob Senecal <jsenecal@figure.com>
2021-01-13 17:14:24 -0700

    add feed reducer

--- frontend_rss_feed/src/actions/feed-actions.js
+++ frontend_rss_feed/src/actions/feed-actions.js
@@ -1,9 +1,9 @@
 import { ajaxGet } from "./ajax";
 
-export const FETCH = "FETCH";
+export const GET_FEED = "GET_FEED";
 
 const FEED_CONTEXT = "/api/v1/get-feeds";
 
 export const getFeeds = () => async (dispatch) => {
-    ajaxGet(FETCH, dispatch, FEED_CONTEXT);
+    ajaxGet(GET_FEED, dispatch, FEED_CONTEXT);
 }
---
+++ frontend_rss_feed/src/reducers/feedReducer.js
@@ -0,0 +1,27 @@
+import { REQUEST, SUCCESS, FAILURE } from "../actions/http-actions";
+import { GET_FEED } from "../actions/feed-actions";
+
+const feedReducer = (state, action) => {
+    switch(action.type) {
+        case `${GET_FEED}_${REQUEST}`:
+            return {...state, isLoading: true, isError: false};
+        case `${GET_FEED}_${SUCCESS}`:
+            return {
+                ...state,
+                isLoading: false,
+                isError: false,
+                data: action.payload,
+            };
+        case `${GET_FEED}_${FAILURE}`:
+            return {
+                ...state,
+                isLoading: false,
+                isError: true,
+            };
+        default:
+            throw new Error();
+    }
+}
+
+export default feedReducer;
+
