ae6078a01760349182eb50a1e84dd7266fbfc9fa
Jacob Senecal <jsenecal@figure.com>
2021-02-07 11:48:09 -0700

    add feed search hook

--- frontend_rss_feed/src/actions/feed-search-actions.js
+++ frontend_rss_feed/src/actions/feed-search-actions.js
@@ -4,6 +4,6 @@ export const SUBMIT_FEED_SEARCH = "SUBMIT_FEED_SEARCH";
 
 const FEED_SEARCH_CONTEXT = "/api/v1/feeds/search-feeds";
 
-export const submitFeedSearch = (searchTerm) => async (dispatch) => {
+export const submitFeedSearch = (dispatch, searchTerm) => {
     ajaxGet(SUBMIT_FEED_SEARCH, dispatch, `${FEED_SEARCH_CONTEXT}?searchTerm=${searchTerm}`)
 }
---
+++ frontend_rss_feed/src/hooks/useFeedSearch.js
@@ -0,0 +1,19 @@
+import { useEffect, useReducer } from "react";
+import { submitFeedSearch } from "../actions/feed-search-actions";
+import feedSearchReducer from "../reducers/feedSearchReducer";
+
+const useFeedSearch = (searchTerm) => {
+    const [state, dispatch] = useReducer(feedSearchReducer, {
+        isLoading: false,
+        isError: false,
+        data: {}
+    });
+
+    useEffect(() => {
+        submitFeedSearch(dispatch, searchTerm)
+    }, [dispatch, searchTerm]);
+
+    return state
+}
+
+export default useFeedSearch;
